3a82accb07c4fe48b8f817eb0a9f4b957e9eed79
Spencer <spencerkohler52507@gmail.com>
2021-10-13 09:48:51 -0700

    Add First & Last name variables
    We made the first and last name name variables, and then tested them.

--- Control flow.py
+++ Control flow.py
@@ -1,3 +1,17 @@
 #Programer: Spencer Kohler
 #Date: 10-11-2021
-#Program: Control flow
+#Program: ATM Bank Transaction
+
+"""
+This program simulates an ATM utilizing if, elif, and else statements
+Nesting if statments and refresh our comparison and logical operators
+"""
+
+print("Welcome to Cash R us!\nLet's take a moment to set up your account.\n")
+
+#Set up account by finding the their first and last names using Variables
+
+firstName = input("What is your first name: ")
+lastName = input("What is your last name: ")
+
+print("Welcome to Cash R Us,", firstName, lastName + ", we will now set up a pin for your account.\n")
